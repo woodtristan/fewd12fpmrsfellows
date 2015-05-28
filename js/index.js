4b204e84d6b090c7b9abcdbe7318e8966abf1d5c
woodtristan <wood.tristan@gmail.com>
2015-05-27 20:54:07 -0400

    Final Project Files
    Initial Files

---
+++ js/index.js
@@ -0,0 +1,17 @@
+ $(document).ready(function(){
+ 	$('.carousel').slick({
+ 		autoplay: true
+ 	});
+ });
+
+
+
+/*PAGE 2
+
+submit form upon click of submit button
+
+PAGE 3 
+
+pull in calendar API 
+
+create a pop-up box with event details when someone clicks an event in the calendar*/
---
+++ js/pg3index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        googleCalendarApiKey: '<AIzaSyD4cL2GHcoj0My3gCmcJV8eowEABmolsxQ',
+        events: {
+            googleCalendarId: 'dt7hpfr9gtn0npluv172cmgmj4@group.calendar.google.com'
+        }
+    });
+});
+
+
+
+
+/*PAGE 2
+
+submit form upon click of submit button
+
+PAGE 3 
+
+pull in calendar API 
+
+create a pop-up box with event details when someone clicks an event in the calendar*/
